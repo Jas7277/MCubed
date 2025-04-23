91288a807b40cd8642379c6aa709ab0b500b0720
Jason <jasonsworld21@yahoo.com>
2025-04-22 21:08:56 -0400

    Reconfigured project

---
+++ out/build/MCubedPreset/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jason/Documents/MCubed
+# Build directory: C:/Users/jason/Documents/MCubed/out/build/MCubedPreset
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
