eada75a01c50203c4c2db7723ff8205a650fc752
jason <jasonsworld21@yahoo.com>
2025-10-29 14:26:11 -0400

    Cleaned up some warnings.

--- src/main/java/jas7277/View/LeftPanel.java
+++ src/main/java/jas7277/View/LeftPanel.java
@@ -7,8 +7,6 @@ import jas7277.Model.ServerTypes;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Objects;
 
