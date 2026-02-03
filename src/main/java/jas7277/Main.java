abd062f398b9b784eab78d2d1d8558770525e8a0
jason <scottjas595@gmail.com>
2026-02-03 10:10:56 -0500

    Cleaned up the main file warnings.

--- src/main/java/jas7277/Main.java
+++ src/main/java/jas7277/Main.java
@@ -1,10 +1,7 @@
 import javax.swing.SwingUtilities;
 
 import jas7277.View.Frame;
-import jas7277.Model.FileManager;
 
-import java.io.File;
-
-public static void main() {
+void main() {
     SwingUtilities.invokeLater(new Frame());
 }
